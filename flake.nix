426aaf7497f1616240cdf2c67e83b3c0ef7c9e35
cyan.lin <xusine@outlook.com>
2023-06-23 16:00:52 +0000

    chore: add flake.nix for easy environment setup

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A flake for building QEMU + QFlex";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-22.05";
+
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in 
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        pkgs.gcc
+        pkgs.ninja
+        pkgs.glib
+        pkgs.pkg-config
+        pkgs.pixman
+        pkgs.capstone
+      ];
+    };
+
+    package.${system}.default = pkgs.stdenv.mkDerivation {
+      name = "QEMU QFLex";
+      src = ./.;
+      buildPhase = "./configure --target-list=aarch64-softmmu --disable-gtk --enable-sanitizers --enable-capstone && cd build && ninja && cd ..";
+    };
+
+  };
+}
